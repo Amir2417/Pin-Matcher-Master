27c3ab2933fe4c061661059b944098a56140516f
Amir2417 <amirsohelto@gmail.com>
2022-01-11 21:05:11 +0600

    typed number show in the input field

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
-function getPin(){
-    const pin = Math.round(Math.random() * 10000);
-    const pinString = pin + '';
-    if(pinString.length == 4){
-        return pin;
-    }
-    else{
-        return getPin();
-    }
-    
+// randomly generate the pin
+
+function getPin() {
+  const pin = Math.round(Math.random() * 10000);
+  const pinString = pin + "";
+  if (pinString.length == 4) {
+    return pin;
+  } else {
+    return getPin();
+  }
 }
-function generatePin(){
-    const pin = getPin();
-    document.getElementById('display-pin').value = pin;
+function generatePin() {
+  const pin = getPin();
+  document.getElementById("display-pin").value = pin;
 }
+// typed number show
+document.getElementById("key-pad").addEventListener("click", function (event) {
+  const number = event.target.innerText;
+  const typedNumbers = document.getElementById("typed-numbers");
+  //check whether the variable is number or not in NotAnNumber function
+  if (isNaN(number)) {
+    if (number == "C") {
+      typedNumbers.value = "";
+    }
+  } else {
+    const previousValue = typedNumbers.value;
+    const newValue = previousValue + number;
+    typedNumbers.value = newValue;
+  }
+});
